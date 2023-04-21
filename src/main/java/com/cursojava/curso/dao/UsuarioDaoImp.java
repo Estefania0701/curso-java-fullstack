f78dec7763d2c80c8aeed2589de99cdb6dd22d92
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-21 10:22:13 -0500

    Agrego comentarios más detallados para comprender el funcionamiento del código

--- src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
+++ src/main/java/com/cursojava/curso/dao/UsuarioDaoImp.java
@@ -1,3 +1,8 @@
+/* ------------- NOTA
+Este código define una implementación concreta de la interfaz UsuarioDao que
+permite acceder a los datos de usuarios almacenados en una base de datos a
+través de la biblioteca de persistencia de Java llamada JPA (Java Persistence API).*/
+
 package com.cursojava.curso.dao;
 
 import com.cursojava.curso.models.Usuario;
@@ -8,21 +13,74 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-@Repository // funcionalidad de acceder a la BBDD
-@Transactional // forma en que tratará las consultas de SQL (fragmentos de transacción)
+
+/*
+--- Se utiliza para marcar una clase como componente de acceso a datos
+(DAO, por sus siglas en inglés).
+--- Indica que la clase es responsable de interactuar con la capa de
+persistencia de la aplicación, es decir, que se encarga de acceder a la
+base de datos y realizar operaciones CRUD (crear, leer, actualizar y eliminar)
+en las entidades.
+--- Cuando se aplica la anotación @Repository a una clase, Spring la convierte
+en un componente gestionado y administra su ciclo de vida, lo que significa
+que se encarga de crear instancias de la clase y destruirlas cuando ya no son
+necesarias.
+--- También permite que la clase pueda ser escaneada automáticamente por
+Spring y agregada al contexto de la aplicación. Esto significa que la clase
+UsuarioDaoImp se puede utilizar en otras partes de la aplicación sin necesidad
+de crear una instancia manualmente.
+*/
+@Repository
+
+/*
+--- Al aplicarla a nivel de clase, se asegura que todos los métodos públicos
+de la clase se ejecuten dentro de una transacción
+--- Una transacción es una unidad lógica de trabajo que agrupa un conjunto de
+operaciones de base de datos. Todas las operaciones dentro de una transacción
+se ejecutan como una unidad atómica, lo que significa que si una operación
+falla, todas las operaciones realizadas dentro de la transacción se deshacen
+o "deshacen" y se vuelve al estado anterior a la transacción.
+--- En el contexto de esta clase, significa que todos los métodos públicos que
+interactúan con la base de datos, incluyendo el método getUsuarios(), se
+ejecutarán dentro de una transacción. Esto asegura que todas las operaciones
+realizadas dentro de estos métodos se completen correctamente o se deshagan en
+caso de errores, lo que garantiza la integridad y coherencia de los datos en
+la base de datos.
+*/
+@Transactional
 public class UsuarioDaoImp implements UsuarioDao{
 
+    /* Es una anotación de JPA (Java Persistence API) que se utiliza para
+    inyectar un objeto de tipo EntityManager en una clase. */
     @PersistenceContext
-    private EntityManager entityManager; // Para hacer la conexión con la BBDD
+    /* El EntityManager es una interfaz que proporciona una API para realizar
+    operaciones CRUD en la base de datos, así como otras operaciones avanzadas
+    como consultas personalizadas y transacciones.
+    Está diseñado para ser utilizado en el ámbito de una transacción, por lo
+    que se debe garantizar que todas las operaciones realizadas con el
+    EntityManager se realicen dentro del contexto de una transacción.*/
+    private EntityManager entityManager;
 
+    // Sobrescribe el método getUsuarios() definido en la interfaz UsuarioDao
     @Override
+    /* De acuerdo con ChatGPT no es necesario volver a poner la anotación
+    aquí si ya la había puesto a nivel de la clase y si además no estoy
+    definiendo un comportamiento de transacción diferente para este método en
+    particular. Sin embargo, la dejaré solo porque en el curso lo hacen... */
     @Transactional
     public List<Usuario> getUsuarios() {
+        // Selecciono todos los usuarios de la tabla Usuario
         String query = "FROM Usuario";
 
-        // listado de usuarios (getResultList para transformar en lista)
+        /* Ejecuta la consulta definida en la variable query utilizando el que
+        se ha inyectado en la clase con la anotación @PersistenceContext. La
+        consulta se ejecuta utilizando el método createQuery() del EntityManager,
+        que devuelve un objeto de tipo Query. A continuación, se llama al
+        método getResultList() de ese objeto para obtener una lista de objetos
+        Usuario. La lista se almacena en la variable resultado.*/
         List<Usuario> resultado = entityManager.createQuery(query).getResultList();
 
+        // Devuelvo la lista de usuarios previamente obtenida
         return resultado;
     }
 }
