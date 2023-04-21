315da3fe774dcc5bd07df57d6d32df8bfac7ea07
Estefanía Aguas <estefaniaaguas@gmail.com>
2023-04-21 16:22:30 -0500

    Agrego la librería Argon2 para hashear las contraseñas

--- src/main/java/com/cursojava/curso/controllers/UsuarioController.java
+++ src/main/java/com/cursojava/curso/controllers/UsuarioController.java
@@ -20,6 +20,8 @@ package com.cursojava.curso.controllers;
 
 import com.cursojava.curso.dao.UsuarioDao;
 import com.cursojava.curso.models.Usuario;
+import de.mkammerer.argon2.Argon2;
+import de.mkammerer.argon2.Argon2Factory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -99,6 +101,21 @@ public class UsuarioController {
     usuarioDao.*/
     @RequestMapping(value = "api/usuarios", method = RequestMethod.POST)
     public void registrarUsuario(@RequestBody Usuario usuario){
+
+        /* Creo una instancia de la clase Argon2, que es una biblioteca de
+        hash de contraseñas que utiliza un algoritmo seguro y resistente
+        a los ataques de fuerza bruta.*/
+        Argon2 argon2 = Argon2Factory.create(Argon2Factory.Argon2Types.ARGON2id);
+
+        /* Genero un hash seguro de la contraseña del usuario, utilizando
+        el método hash(). Este método toma cuatro parámetros: el número de
+        iteraciones, la memoria requerida, el número de hilos y la
+        contraseña en sí.*/
+        String hash = argon2.hash(1, 1024, 1, usuario.getPassword());
+
+        // Reasigno la contraseña con el hash generado
+        usuario.setPassword(hash);
+
         usuarioDao.registrar(usuario);
     }
 
